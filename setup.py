6d7a34796cd80231a7146449a633417b69545a66
andretocci <andrerussi002@gmail.com>
2020-11-04 09:20:29 -0300

    new setup

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 
 
 setup(
-    name='statcc',
-    version='0.0.1',
+    name='testes_hipoteses',
+    version='0.0.2',
     description='Estudos de Estatistica',
     long_description='long_description',
     long_description_content_type="text/markdown",
     author='Andre Tocci',
     author_email='andrerussi002@gmail.com',
     url='https://github.com/andretocci/statcc',
-    packages=['statcc'],
+    packages=['testes'],
+    package_dir={'testes': 'stacc/testes'},
+    package_data={'testes': ['stacc/testes/*.py']},
     install_requires=['numpy', 'scipy' ],
     license="Apache License 2.0",
     classifiers=[
