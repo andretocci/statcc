e0700b9ebdad896d7a3f59555a32e6edf5709c92
andretocci <andrerussi002@gmail.com>
2020-11-04 09:01:28 -0300

    init

--- statcc/__init__.py
+++
@@ -1,1 +0,0 @@
-from .statcc import statcc
---
+++ statcc/testes/__init__.py
@@ -0,0 +1,5 @@
+# from .statcc import statcc
+from .teste_hipoteses import teste_hipoteses
+from .hipoteses_z import hipoteses_z
+from .hipoteses_t import hipoteses_t
+from .hipoteses_proporcao import hipoteses_proporcao
