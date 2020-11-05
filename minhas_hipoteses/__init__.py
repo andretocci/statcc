2f61e14f281fc7381489be695756dbb3e8892748
andretocci <andrerussi002@gmail.com>
2020-11-05 14:20:50 -0300

    import scipy.stats

--- minhas_hipoteses/__init__.py
+++ minhas_hipoteses/__init__.py
@@ -3,3 +3,4 @@ from .teste_hipoteses import teste_hipoteses
 from .hipoteses_z import hipoteses_z
 from .hipoteses_t import hipoteses_t
 from .hipoteses_proporcao import hipoteses_proporcao
+import scipy.stats
