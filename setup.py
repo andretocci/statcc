b9da3ff763aac6b9587d65c8a909781e34789b77
andretocci <andrerussi002@gmail.com>
2020-11-05 14:18:32 -0300

    scipy.stats dependencias

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=setuptools.find_packages(),
     #package_dir={'minhas_hipoteses': 'stacc/minhas_hipoteses'},
     #package_data={'minhas_hipoteses': ['stacc/minhas_hipoteses/*.py']},
-    install_requires=['numpy', 'scipy.stats' ],
+    install_requires=['numpy', 'scipy'],
     license="Apache License 2.0",
     classifiers=[
         "Programming Language :: Python :: 3",
